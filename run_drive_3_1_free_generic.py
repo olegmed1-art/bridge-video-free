f76c483de3a06bf5292cbbd58186a2e5a73f1dbb
olegmed1-art <olegmed1@gmail.com>
2026-08-12 08:11:01 +0300

    Route production 3.1 FREE through semantic transcript QC

--- run_drive_3_1_free_generic.py
+++ run_drive_3_1_free_generic.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Token-provider adapter for Bridge Video 3.1 FREE master analysis."""
-import run_master_3_1_free as master
+import run_master_3_1_free_semantic as master
 
 
 def main(token_func):
