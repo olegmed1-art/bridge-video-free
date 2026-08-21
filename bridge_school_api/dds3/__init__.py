7e52237b5f2c16f146dcb0fe25abf38b7bb4f2b8
olegmed1-art <olegmed1@gmail.com>
2026-08-21 19:37:00 +0300

    Export DDS3 position solver API

--- bridge_school_api/dds3/__init__.py
+++ bridge_school_api/dds3/__init__.py
@@ -1,6 +1,34 @@
 """Bridge School DDS3 public computation boundary."""
 from .image_ingress import ImageEnvelope, ImageIngressError, solve_image_envelope
 from .model import BridgeDeal, DealValidationError
+from .position_runtime import (
+    DDS3PositionConfig,
+    PositionWorker,
+    PositionWorkerUnavailable,
+    solve_position_all_moves,
+    solve_position_trajectory,
+)
 from .screenshot import ObservedField, ScreenshotDealObservation, derive_board_metadata
 from .service import DDS3Config, DDSUnavailable, compute, solve_deal, solve_screenshot_observation, solve_table
-__all__=["ImageEnvelope","ImageIngressError","solve_image_envelope","BridgeDeal","DealValidationError","ObservedField","ScreenshotDealObservation","derive_board_metadata","DDS3Config","DDSUnavailable","compute","solve_deal","solve_screenshot_observation","solve_table"]
+
+__all__ = [
+    "ImageEnvelope",
+    "ImageIngressError",
+    "solve_image_envelope",
+    "BridgeDeal",
+    "DealValidationError",
+    "ObservedField",
+    "ScreenshotDealObservation",
+    "derive_board_metadata",
+    "DDS3PositionConfig",
+    "PositionWorker",
+    "PositionWorkerUnavailable",
+    "solve_position_all_moves",
+    "solve_position_trajectory",
+    "DDS3Config",
+    "DDSUnavailable",
+    "compute",
+    "solve_deal",
+    "solve_screenshot_observation",
+    "solve_table",
+]
