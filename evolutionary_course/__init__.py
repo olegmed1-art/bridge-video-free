8073cd9ae8e31164b4d52f34669d6409528311bf
olegmed1-art <olegmed1@gmail.com>
2026-08-30 08:45:05 +0300

    feat: expose Video 3.1 Course v1 adapter

--- evolutionary_course/__init__.py
+++ evolutionary_course/__init__.py
@@ -11,14 +11,22 @@ from .contract import (
     canonical_sha256,
     validate_episode,
 )
+from .video31_adapter import (
+    ADAPTER_SCHEMA,
+    Video31AdapterError,
+    adapt_video31_quality,
+)
 
 __all__ = [
+    "ADAPTER_SCHEMA",
     "AUTHORITY_CLASS",
     "COURSE_VERSION",
     "EPISTEMIC_CLASSES",
     "EpisodeContractError",
     "SCHEMA",
     "SKILL_STATES",
+    "Video31AdapterError",
+    "adapt_video31_quality",
     "build_skill_trajectory",
     "canonical_sha256",
     "validate_episode",
