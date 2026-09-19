5c49524eb22ee53907c082e49a0b21b1c43841e7
olegmed1-art <olegmed1@gmail.com>
2026-09-19 18:45:51 +0300

    test(broker): pin role dispatch endpoint to mailbox v3

--- autopilot_token_broker_service/tests/test_role_dispatch_contract.py
+++ autopilot_token_broker_service/tests/test_role_dispatch_contract.py
@@ -372,8 +372,8 @@ class RoleDispatchContractTests(unittest.TestCase):
         )
         _authorize_github_operation(method="GET", path=allowed)
         for method, path in (
-            ("POST", f"{REPOSITORY_API_PATH}/issues/1637/comments"),
-            ("GET", f"{REPOSITORY_API_PATH}/issues/1637"),
+            ("POST", f"{REPOSITORY_API_PATH}/issues/1685/comments"),
+            ("GET", f"{REPOSITORY_API_PATH}/issues/1685"),
             ("PATCH", allowed),
             ("DELETE", allowed),
         ):
